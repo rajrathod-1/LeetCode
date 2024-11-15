959781dad1a185ffc5cb30f3c7e7ec39cf95495c
rajrathod-1 <rajrathodwork@gmail.com>
2024-11-15 01:46:45 -0600

    Time: 0 ms (100.00%) | Memory: 42 MB (99.83%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -2,19 +2,15 @@ class Solution {
     public int searchInsert(int[] nums, int target) {
         int start = 0;
         int end = nums.length - 1;
-        int index = end + 1;
+        int ans = nums.length;
 
         int mid = 0;
         while(start <= end)
         {
             mid = start + (end - start)/2;
-            if(target == nums[mid])
+             if(target <= nums[mid])
             {
-                return mid;
-            }
-            else if(target < nums[mid])
-            {
-                index = mid;
+                ans = mid;
                 end = mid - 1;
             }
 
@@ -23,6 +19,6 @@ class Solution {
                 start = mid + 1;
             }
         }
-        return index;
+        return ans;
     }
 }
