8906f3564f0c371536ee5cc69526b2d951e410b4
rajrathod-1 <rajrathodwork@gmail.com>
2024-12-18 23:42:34 -0600

    Time: 0 ms (100.00%) | Memory: 42.5 MB (5.01%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,34 +1,32 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int count0 = 0, count1 = 0, count2 = 0;
+        int start = 0;
+        int mid = 0;
+        int end = nums.length - 1;
 
-        // Count the occurrences of 0s, 1s, and 2s
-        for (int num : nums) {
-            if (num == 0) {
-                count0++;
-            } else if (num == 1) {
-                count1++;
-            } else if (num == 2) {
-                count2++;
+        while(mid <= end)
+        {
+            if(nums[mid] == 0)
+            {
+                int temp = nums[mid];
+                nums[mid] = nums[start];
+                nums[start] = temp;
+                start++;
+                mid++;
             }
+            else if(nums[mid] == 1)
+            {
+                mid++;
+            }
+            else
+            {
+                int temp = nums[end];
+                nums[end] = nums[mid];
+                nums[mid] = temp;
+                end--;
+            }
+            
         }
-
-        // Fill the array with 0s, then 1s, then 2s based on the counts
-        int index = 0;
-
-        // Fill with 0s
-        for (int i = 0; i < count0; i++) {
-            nums[index++] = 0;
-        }
-
-        // Fill with 1s
-        for (int i = 0; i < count1; i++) {
-            nums[index++] = 1;
-        }
-
-        // Fill with 2s
-        for (int i = 0; i < count2; i++) {
-            nums[index++] = 2;
-        }
+        
     }
-}
+};
