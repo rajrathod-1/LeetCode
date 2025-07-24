5643d0a6cd1fc582bc116c8de50195a813247776
Raj Rathod <rajrathodwork@gmail.com>
2025-07-23 22:21:16 -0500

    Time: 3 ms (99.79%) | Memory: 63 MB (82.74%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
@@ -10,34 +10,28 @@
  */
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
-
-            // Edge case: Single node in the list
-        if (head == null || head.next == null) {
-            return null;
-        }
-
-        // Count the total number of nodes
+        if(head == null)  return null;
         int count = 0;
         ListNode temp = head;
-        while (temp != null) {
+
+        while(temp != null)
+        {
             count++;
             temp = temp.next;
         }
+        int mid = count/2;
 
-        // Find the middle position
-        int middle = count / 2;
-
-        // Traverse to the node just before the middle
-        ListNode prev = null;
-        ListNode curr = head;
-        for (int i = 0; i < middle; i++) {
-            prev = curr;
-            curr = curr.next;
+        temp = head;
+        while(temp != null)
+        {
+            mid--;
+            if(mid == 0)
+            {
+                temp.next = temp.next.next;
+                return head;
+            }
+            temp = temp.next;
         }
-
-        // Skip the middle node
-        prev.next = curr.next;
-
-        return head;
+        return null;
     }
 }
